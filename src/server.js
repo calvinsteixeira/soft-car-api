c132ed20bb45a79751bbb5caf670bb51dfd38277
Calvin Teixeira <calvingsx@gmail.com>
2022-03-11 09:48:10 -0300

    Importing database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes/routes')
+const cors = require('cors')
+const database = require('./models/database')
 const app = express()
 const router = express.Router()
-const cors = require('cors')
+
 
 app.use(cors())
 app.use('/', router)
