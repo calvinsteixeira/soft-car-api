a730b822415f7fd2a6f8e960302e1c2b14a6382e
Calvin Teixeira <calvingsx@gmail.com>
2022-03-11 11:58:05 -0300

    Small fix

--- src/models/database.js
+++ src/models/database.js
@@ -1,4 +1,4 @@
-const { Sequelize, Model, DataTypes } = require('sequelize')
+const Sequelize  = require('sequelize')
 const sequelize = new Sequelize('authenticate_database', 'admin', '@m4rk3t1ng', {
   host: 'localhost',
   dialect: 'mysql'
@@ -6,5 +6,5 @@ const sequelize = new Sequelize('authenticate_database', 'admin', '@m4rk3t1ng', 
 
 sequelize.authenticate().then(() => console.log('Conexão com o banco de dados realizada com sucesso')).catch(() => console.log('Falha na conexão com o banco de dados'))
 
-module.export = sequelize
+module.exports = sequelize;
 
