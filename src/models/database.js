8a60ca9faeb2aaa04dea0fa6dc44d901bc992ee9
Calvin <calvingsx@gmail.com>
2022-03-11 13:23:42 -0300

    Small change

--- src/models/database.js
+++ src/models/database.js
@@ -1,10 +1,17 @@
-const Sequelize  = require('sequelize')
-const sequelize = new Sequelize('authenticate_database', 'admin', '@m4rk3t1ng', {
-  host: 'localhost',
-  dialect: 'mysql'
-})
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(
+  'authenticate_database',
+  'calvin',
+  'calvintex7',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+)
 
-sequelize.authenticate().then(() => console.log('Conexão com o banco de dados realizada com sucesso')).catch(() => console.log('Falha na conexão com o banco de dados'))
+sequelize
+  .authenticate()
+  .then(() => console.log('Conexão com o banco de dados realizada com sucesso'))
+  .catch(() => console.log('Falha na conexão com o banco de dados'))
 
-module.exports = sequelize;
-
+module.exports = sequelize
