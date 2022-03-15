aecee02d61dc0c194460010e33b63aa11c51a26c
Calvin Teixeira <calvingsx@gmail.com>
2022-03-15 09:08:46 -0300

    Creating register-user route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,14 +3,19 @@ const User = require('../models/user')
 module.exports = function (router) {
   const bodyParser = require('body-parser')
   router.use(bodyParser.json({ extended: true }))
-  const authController = require('../controller/authController')
 
   // AUTH ROUTE
+  const authController = require('../controller/authController')
   router.post('/auth', async (req, res) => {
     const credentials = req.body
     const response = await authController.login(credentials)
     res.send(response)
   })
 
- 
+  // REGISTER NEW USER ROUTE
+  const userController = require('../controller/userController')
+  router.post('/register-user', async (req, res) => {
+    const result = await userController.register(req.body)
+    res.send(result)
+  })
 }
