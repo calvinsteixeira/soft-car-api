36eb274449477a4a35f807823d6001953112d58b
Calvin Teixeira <calvingsx@gmail.com>
2022-03-15 17:52:09 -0300

    Creating /get-users route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,8 +11,7 @@ module.exports = function (router) {
   // AUTH ROUTE
 
   router.post('/auth', async (req, res) => {
-    const credentials = req.body
-    const response = await authController.login(credentials)
+    const response = await authController.login(req.body)
     res.status(response.statusCode).send(response)
   })
 
@@ -20,7 +19,13 @@ module.exports = function (router) {
 
   router.post('/register-user', async (req, res) => {
     const response = await userController.register(req.body)
-    // res.status(response.statusCode).send(response)
+    res.status(response.statusCode).send(response)
+  })
+
+  // GET USERS ROUTE
+
+  router.post('/get-users', async (req, res) => {
+    const response = await userController.getUsers(req.body)
     res.status(response.statusCode).send(response)
   })
 }
