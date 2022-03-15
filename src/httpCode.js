da10283d7c51570ebc75bae0b42dddaf626785fa
Calvin Teixeira <calvingsx@gmail.com>
2022-03-15 09:45:28 -0300

    Creating http codes collection

---
+++ src/httpCode.js
@@ -0,0 +1,19 @@
+const httpCode = [
+  {
+    statusCode: 200,
+    errorMessage: "OK"
+  },
+  {
+    statusCode: 404,
+    errorMessage: "RESOURCE NOT FOUND"
+  },
+  {
+    statusCode: 401,
+    errorMessage: "INVALID CREDENTIALS"
+  },
+  {
+    statusCode: 400,
+    errorMessage: "BAD REQUEST"
+  }
+]
+
