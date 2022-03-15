4117b3f8cf064eb8ca0cb6c4c5f546b38de63b67
Calvin Teixeira <calvingsx@gmail.com>
2022-03-15 14:46:53 -0300

    Changing some query and verification rules

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,21 +4,32 @@ module.exports = {
   login: async clientCredentials => {
     const dbCredentials = await User.findOne({
       where: {
-        username: clientCredentials.username,
-        password: clientCredentials.password
+        username: clientCredentials.username
       },
       attributes: ['username', 'password']
     })
 
     if (!dbCredentials) {
-      return 'Usuário ou senha incorreto'
+      return {
+        hasError: true,
+        statusCode: 404,
+        message: 'User not found'
+      }
     } else if (
       (clientCredentials.username === dbCredentials.username) &
       (clientCredentials.password === dbCredentials.password)
     ) {
-      return 'Autenticação válida'
+      return {
+        hasError: false,
+        statusCode: 200,
+        message: 'Login sucessfull'
+      }
+    } else {
+      return {
+        hasError: true,
+        statusCode: 401,
+        message: 'Invalid credentials'
+      }
     }
   }
 }
-
-
