02f8e03180cd4b1bf61d5570762ffc8c2fe3e082
Calvin Teixeira <calvingsx@gmail.com>
2022-03-15 17:49:45 -0300

    Exporting register method

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,5 +4,9 @@ module.exports = {
   register: async (newUser) => {
     const result = await userService.register(newUser)
     return result
+  },
+  getUsers: async (user) => {
+    const result = await userService.getUsers(user)
+    return result
   }
 }
