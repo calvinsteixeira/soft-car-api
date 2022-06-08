ab50dc5ffb79a6195d62d9c4f18a858e5ac868f8
Calvin Teixeira <calvin.teixeira@avmb.com.br>
2022-06-08 17:34:23 -0300

    Adding Db url from heroku db

--- src/models/database.js
+++ src/models/database.js
@@ -1,21 +1,27 @@
-const Sequelize = require('sequelize')
-const Op = Sequelize.Op
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_TYPE
-  }
-)
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_TYPE,
+      port: 3306,
+    }
+  );
+}
 
 sequelize
   .authenticate()
-  .then(() => console.log('Conexão com o banco dados: OK'))
-  .catch(() => console.log('Conexão com o banco de dados: FALHA'))
+  .then(() => console.log("Conexão com o banco dados: OK"))
+  .catch(() => console.log("Conexão com o banco de dados: FALHA"));
 
 module.exports = {
   sequelize,
-  Op
-}
+  Op,
+};
