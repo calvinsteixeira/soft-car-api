6fef9eb62fc3549757cf62b4c45353bc8e0da490
Calvin <calvingsx@gmail.com>
2022-06-08 20:46:39 -0300

    Small change

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,49 +1,49 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
 async function login(clientCredentials) {
-  const bcrypt = require('../utils/bcrypt')
+  const bcrypt = require("../utils/bcrypt");
 
   const dbCredentials = await User.findOne({
     where: {
-      username: clientCredentials.username
+      username: clientCredentials.username,
     },
-    attributes: ['username', 'password']
-  })
+    attributes: ["username", "password"],
+  });
 
   if (!dbCredentials) {
     return {
       statusCode: 404,
       data: {
         hasError: true,
-        message: 'Usuário não cadastrado'
-      }
-    }
+        message: "Usuário não cadastrado",
+      },
+    };
   } else {
     const match = await bcrypt.comparePassword(
       clientCredentials.password,
       dbCredentials.password
-    )
+    );
 
     if (match) {
       return {
         statusCode: 200,
         data: {
           hasError: false,
-          message: 'Login realizado com sucesso'
-        }
-      }
+          message: "Login realizado com sucesso",
+        },
+      };
     } else {
       return {
         statusCode: 401,
         data: {
           hasError: true,
-          message: 'Usuário ou senha incorretos'
-        }
-      }
+          message: "Usuário ou senha incorretos",
+        },
+      };
     }
   }
 }
 
 module.exports = {
-  login
-}
+  login,
+};
