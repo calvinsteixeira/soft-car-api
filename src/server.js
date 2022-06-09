63fb05c2e24a60f3d8e2c30dd1fea43268175fb7
Calvin <calvingsx@gmail.com>
2022-06-08 21:39:57 -0300

    Adding cors options

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
-require('dotenv').config()
-const express = require('express')
-const routes = require('./routes/routes')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const routes = require("./routes/routes");
+const cors = require("cors");
 
-const app = express()
-const router = express.Router()
+const app = express();
+const router = express.Router();
 
-app.use(express.json())
-app.use(cors())
-app.use('/', router)
-routes(router)
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use("/", router);
+routes(router);
 
 // INICIANDO O SERVIDOR
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
